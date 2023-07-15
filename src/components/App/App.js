beab58b35243db0fe8dc904ef0be4dd11719cd35
Steve Shumaker <steve.j.shumaker@gmail.com>
2023-07-15 11:17:33 -0500

    working GET request to giphy API

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
 
 function App(props) {
+  const fetchGif = () => {
+    fetch(`/api/gifs`)
+      .then((response) => {
+        console.log("fetching...");
+        const gif = response.json();
+        console.log('CLIENT GIF DATA ----> ',gif);
+        return gif;
+        // console.log("GIF DATA ----> ", response);
+      })
+      .then((data) => {
+        console.log('CLIENT DATA --> ', data.data);
+      });
+  };
+
   return (
     <div>
       <h1>Giphy Search!</h1>
+      <button onClick={fetchGif}>Fetch Gif</button>
     </div>
   );
 }
