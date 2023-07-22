d8990633d2a38c2b20c784b786333390c62e9c0a
Steve Shumaker <steve.j.shumaker@gmail.com>
2023-07-22 10:39:41 -0500

    first go at get and put routes for favorites

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -5,7 +5,20 @@ const router = express.Router();
 
 // return all favorite images
 router.get("/", (req, res) => {
-  res.sendStatus(200);
+  const QUERY = `SELECT * FROM favorites;`;
+
+  pool
+    .query(QUERY)
+    .then((response) => {
+      response.json();
+    })
+    .then((favGifs) => {
+      res.send(favGifs).status(200);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
 });
 
 // add a new favorite
@@ -13,13 +26,26 @@ router.post("/", (req, res) => {
   const { title, username, gif, category } = req.body;
   const QUERY = `INSERT INTO favorites (title, username, gif) VALUES ($1, $2, $3);`;
 
-  pool.query(QUERY, [title, username, gif]).then(res.sendStatus(201));
+  pool.query(QUERY, [title, username, gif]).then(
+    res.sendStatus(201).catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    })
+  );
 });
 
 // update given favorite with a category id
 router.put("/:favId", (req, res) => {
   // req.body should contain a category_id to add to this favorite image
-  res.sendStatus(200);
+  const gifId = req.params.favId;
+  const categoryId = req.body;
+
+  const QUERY = `UPDATE favorites SET category_id = $2 WHERE favorites.id = $2;`;
+
+  pool
+    .query(QUERY, [gifId, categoryId])
+    .then((response) => res.send(response).status(200))
+    .catch((err) => console.log(err));
 });
 
 // delete a favorite
