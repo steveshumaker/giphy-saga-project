8ce2c1f36d1f8689781bb773f057e95eff8c5fe7
Luke Gartner <lukegartner4@gmail.com>
2023-07-22 10:14:33 -0500

    sorts favorites by category

--- src/components/GiphyDisplay/FavoritesDisplay.jsx
+++ src/components/GiphyDisplay/FavoritesDisplay.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 // MUI
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
@@ -6,11 +7,14 @@ import ImageListItemBar from "@mui/material/ImageListItemBar";
 import ListSubheader from "@mui/material/ListSubheader";
 import IconButton from "@mui/material/IconButton";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-import { Typography } from "@mui/material";
+import { Container, Typography } from "@mui/material";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 const FavoritesDisplay = () => {
   //   const { giphyFavorites } = useSelector((store) => store);
-
   const giphyFavorites = [
     {
       id: 1,
@@ -21,6 +25,15 @@ const FavoritesDisplay = () => {
     },
   ];
 
+  const [category, setCategory] = useState("");
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("api/category")
+      .then((response) => response.json())
+      .then(setCategories);
+  }, []);
+
   const toggleFavorite = () => {};
 
   return (
@@ -28,34 +41,59 @@ const FavoritesDisplay = () => {
       <Typography variant="h2" align="center">
         Favorites
       </Typography>
+      <Container sx={{ width: "18rem" }}>
+        <FormControl fullWidth>
+          <InputLabel id="favorites-category">Category</InputLabel>
+          <Select
+            labelId="favorites-category-label"
+            id="favorites-category-select"
+            value={category}
+            label="Age"
+            onChange={(event) => setCategory(event.target.value)}
+          >
+            {categories.map((cat) => (
+              <MenuItem key={cat.id} value={cat.id}>
+                {cat.name}
+              </MenuItem>
+            ))}
+            <MenuItem key="all" value="all">
+              All
+            </MenuItem>
+          </Select>
+        </FormControl>
+      </Container>
       <ImageList
         sx={{ width: 500, height: 450, mx: "auto" }}
         cols={3}
         rowHeight={164}
       >
-        {giphyFavorites.map(({ id, title, gif, username, category_id }) => (
-          <ImageListItem key={id}>
-            <img
-              src={`${gif}?w=164&h=164&fit=crop&auto=format`}
-              srcSet={`${gif}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-              alt={title}
-              loading="lazy"
-            />
-            <ImageListItemBar
-              title={title}
-              subtitle={username}
-              actionIcon={
-                <IconButton
-                  onClick={toggleFavorite}
-                  sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                  aria-label={`info about ${title}`}
-                >
-                  <FavoriteIcon />
-                </IconButton>
-              }
-            />
-          </ImageListItem>
-        ))}
+        {giphyFavorites.map(({ id, title, gif, username, category_id }) => {
+          if (category_id === category || category === "all") {
+            return (
+              <ImageListItem key={id}>
+                <img
+                  src={`${gif}?w=164&h=164&fit=crop&auto=format`}
+                  srcSet={`${gif}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                  alt={title}
+                  loading="lazy"
+                />
+                <ImageListItemBar
+                  title={title}
+                  subtitle={username}
+                  actionIcon={
+                    <IconButton
+                      onClick={toggleFavorite}
+                      sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                      aria-label={`info about ${title}`}
+                    >
+                      <FavoriteIcon />
+                    </IconButton>
+                  }
+                />
+              </ImageListItem>
+            );
+          }
+        })}
       </ImageList>
     </>
   );
